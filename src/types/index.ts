d2507fee20a5b62433f3e029fe1d3b5afc7a9249
humengming <humengmingxx@gmail.com>
2021-11-10 23:16:08 +0800

    feat(entry)：init

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type TMethod =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: TMethod
+  data?: any
+  params?: any
+}
