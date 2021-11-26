cf6e9f1724543de3e2c5404ae5bbda88a7ca4fd8
humengming <humengmingxx@gmail.com>
2021-11-26 23:36:38 +0800

    test(helpers)：add headers test & fix parseHeaders bug

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -31,17 +31,17 @@ export const processHeaders = (headers: any, data: any): any => {
 // 处理接口返回的headers
 export const parseHeaders = (headers: string): any => {
   let parsed = Object.create(null)
-  if (!headers)  return parsed;
+  if (!headers) {
+    return parsed
+  }
 
   headers.split('\r\n').forEach(line => {
-    let [key, val] = line.split(':')
+    let [key, ...vals] = line.split(':')
     key = key.trim().toLowerCase()
-
-    if (!key) return;
-    if (val) {
-      val = val.trim()
+    if (!key) {
+      return
     }
-
+    let val = vals.join(':').trim()
     parsed[key] = val
   })
 
