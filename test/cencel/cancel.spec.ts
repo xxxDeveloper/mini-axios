2f6387de7726ecda387cdc29510138eb7e758fe0
humengming <humengmingxx@gmail.com>
2021-11-27 17:38:13 +0800

    test(cancel)：add test

---
+++ test/cencel/cancel.spec.ts
@@ -0,0 +1,17 @@
+import Cancel from '../../src/cancel/Cancel'
+import isCancel from '../../src/cancel/isCancel'
+
+describe('cancel:Cancel', () => {
+  test('should returns correct result when message is specified', () => {
+    const cancel = new Cancel('Operation has been canceled.')
+    expect(cancel.message).toBe('Operation has been canceled.')
+  })
+
+  test('should returns true if value is a Cancel', () => {
+    expect(isCancel(new Cancel())).toBeTruthy()
+  })
+
+  test('should returns false if value is not a Cancel', () => {
+    expect(isCancel({ foo: 'bar' })).toBeFalsy()
+  })
+})
